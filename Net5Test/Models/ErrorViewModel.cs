3d20e28d844733e3b208b72e914dd7eb14c7db3b
Dejun Tu <de-jun.tu@microfocus.com>
2020-11-25 11:29:27 +0800

    test

---
+++ Net5Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net5Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

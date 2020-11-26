50ad1d4b41e39a502a0e11b747a060636d22f946
Dejun Tu <de-jun.tu@microfocus.com>
2020-11-26 11:47:25 +0800

    test

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -6,9 +6,14 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
+        public void Test1() {
+            Assert.True(1 == 1);
+        }
 
+        [Fact]
+        public void SecondTest() {
+            int x = 3;
+            Assert.Equal(x, 3);
         }
     }
 }
